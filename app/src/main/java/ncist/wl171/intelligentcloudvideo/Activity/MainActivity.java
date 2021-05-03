cc21b2c29b5dba27f5fa7edb4f422262bf4c8c51
王震 <1127535653@qq.com>
2021-05-03 21:22:19 +0800

    新增主界面最上面功能栏。 新增我的按钮与推出登录功能。

--- app/src/main/java/ncist/wl171/intelligentcloudvideo/Activity/MainActivity.java
+++ app/src/main/java/ncist/wl171/intelligentcloudvideo/Activity/MainActivity.java
@@ -1,15 +1,61 @@
 package ncist.wl171.intelligentcloudvideo.Activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import ncist.wl171.intelligentcloudvideo.R;
 
+import static ncist.wl171.intelligentcloudvideo.Base.BaseApplication.getOpenSDK;
+
 public class MainActivity extends RootActivity {
 
+    private Button mUserBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mUserBtn = (Button) findViewById(R.id.btn_user);
+        mUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popLogoutDialog();
+            }
+        });
+    }
+
+    private void popLogoutDialog() {
+        AlertDialog.Builder exitDialog = new AlertDialog.Builder(MainActivity.this);
+        exitDialog.setTitle(R.string.exit);
+        exitDialog.setMessage(R.string.exit_tip);
+        exitDialog.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), getString(R.string.tip_login_out), Toast.LENGTH_LONG).show();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getOpenSDK().logout();
+                    }
+                }).start();
+                finish();
+            }
+        });
+        exitDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // doNothing
+            }
+        });
+        exitDialog.show();
     }
 
 }
